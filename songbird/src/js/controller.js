0a62f8f64321e6ff45f6966fd23ab4e1200ac4cb
lgklsv <kolosov20011@gmail.com>
2022-11-09 22:37:20 +0300

    feat: add style to the bird card

--- songbird/src/js/controller.js
+++ songbird/src/js/controller.js
@@ -3,4 +3,4 @@ import secretBird from '../assets/img/bird.06a46938.jpg';
 
 
 document.querySelector('.secret-bird__img').src = secretBird;
-document.querySelector('.bird-card__img_revealed').src = secretBird;
+document.querySelector('.bird-card__img').src = secretBird;
