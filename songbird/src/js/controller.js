0ddca3d409c30af09ef904cc212a2f46f2f299f4
lgklsv <kolosov20011@gmail.com>
2022-11-09 21:15:56 +0300

    feat: add basic html structure

--- songbird/src/js/controller.js
+++ songbird/src/js/controller.js
@@ -1,1 +1,7 @@
 import '../styles/main.scss';
+import secretBird from '../assets/img/bird.06a46938.jpg';
+import logo from '../assets/svg/logo.4f82cd73.svg';
+
+
+document.querySelector('.bird-card__img').src = secretBird;
+document.querySelector('.bird-card__img_revealed').src = secretBird;
--- songbird/webpack.config.js
+++ songbird/webpack.config.js
@@ -48,7 +48,7 @@ module.exports = {
             },
             {
                 test: /\.(png|svg|jpg|jpeg|gif)$/i,
-                type: 'assets/resourse',
+                type: 'asset/resource',
             },
             {
                 test:/\.mp3$/,
