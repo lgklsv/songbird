539ec51a088fb734cdc7f4622baa013c2c88abf5
lgklsv <kolosov20011@gmail.com>
2022-11-13 17:33:00 +0300

    refactor: add some comments to the init fuction and other quality of life improvements

--- songbird/src/js/controller.js
+++ songbird/src/js/controller.js
@@ -8,46 +8,52 @@ import audioView from './views/audioView';
 
 const controlCheckAnswer = function(elem) {
     if(elem.id == model.state.hiddenBird) {
+        const scoreInd = document.querySelector('.header__score-amount');
+        const nextLevelBtn = document.querySelector('.next-btn');
+
+        // Show that the answer is correct
         elem.firstElementChild.classList.add('answers-options__status_right');
         model.state.answered = true;
         const rightAnswer = model.getAnsweredBird(model.state.level, elem.id);
 
+        // Render Answer
         randomBirdView.render(rightAnswer);
         birdCardView.render(rightAnswer);
         model.setSongDurations();
 
-        const scoreInd = document.querySelector('.header__score-amount');
-        const nextLevelBtn = document.querySelector('.next-btn');
-
+        // Set score
         model.state.score = model.state.score + 5 - model.state.missedAnsw;
-
         scoreInd.textContent = model.state.score;
+
+        // Acivate Next level btn
         nextLevelBtn.classList.add('next-btn_active');
     } else {
+        // Remenber if the answer was wrong 
         if(!model.state.answered == true) {
             elem.firstElementChild.classList.add('answers-options__status_wrong');
             model.state.missedAnsw++;
         }
+        // Render Bird
         const answeredBird = model.getAnsweredBird(model.state.level, elem.id)
         birdCardView.render(answeredBird);
         model.setSongDurations();
     }
 }
 
-const initLevel = function() {
+const initLevel = function(level) {
     model.state.answered = false;
     model.state.hiddenBird = null;
     model.state.missedAnsw = 0;
 
-    randomBirdView.render(model.getRandomBird(model.state.level));
-    answersView.render(model.getRandomBirdsArray(model.state.level));
+    randomBirdView.render(model.getRandomBird(level));
+    answersView.render(model.getRandomBirdsArray(level));
     birdCardView.renderMessage('Послушайте плеер.\n Выберите птицу из списка');
     model.setSongDurations();
 }
 
 const init = function() {
-    initLevel();
-    
+    initLevel(model.state.level);
+
     answersView._addHandlerCheckAnswer(controlCheckAnswer);
 }
 init();
